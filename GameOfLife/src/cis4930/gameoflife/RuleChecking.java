01f040117c6b2f62465eab8b31e334b64d87745e
devan <devanp92@ufl.edu>
2014-03-29 18:21:14 -0400

    conditional checking for sides and corners

--- GameOfLife/src/cis4930/gameoflife/RuleChecking.java
+++ GameOfLife/src/cis4930/gameoflife/RuleChecking.java
@@ -25,10 +25,29 @@ public class RuleChecking implements Runnable {
         return listOfSubListsSplitByNumThreads;
     }
 
-    public Cell[][] applyRule(Cell[][] cells){
+    public Cell[][] applyRule(Cell[][] cells) {
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[0].length; j++) {
                 Cell cell = cells[i][j];
+                if (cell.getX() == 0) {
+                    //left side
+                } else if (cell.getX() == cells.length - 1) {
+                    //right side
+                } else if (cell.getY() == 0) {
+                    //top
+                } else if (cell.getY() == cells.length - 1) {
+                    //bottom
+                } else if (cell.getX() == 0 && cell.getY() == 0) {
+                    //top left
+                } else if (cell.getX() == 0 && cell.getY() == cells.length - 1) {
+                    //bottom left
+                } else if (cell.getX() == cells.length - 1 && cell.getY() == 0) {
+                    //top right
+                } else if (cell.getX() == cells.length - 1 && cell.getY() == cells.length - 1) {
+                    //bottom right
+                } else {
+                    //in middle
+                }
             }
         }
         return null;
