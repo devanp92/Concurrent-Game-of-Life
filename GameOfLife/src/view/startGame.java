e0750764356ab999972bda1e6a5e521a79a81fa0
Daniel Fuentes <danielffuentes@gmail.com>
2014-03-29 17:19:53 -0400

    Defined view Package

---
+++ GameOfLife/src/view/startGame.java
@@ -0,0 +1,26 @@
+package view;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Daniel on 3/29/2014.
+ */
+public class startGame extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("startPage.fxml"));
+        //root.getStylesheets().add("sample/myStyle.css");
+        primaryStage.setTitle("Game of Life");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+}
