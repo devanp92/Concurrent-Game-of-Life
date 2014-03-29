6f4bd85e7c280ec13a7cf6e4aef7ba61622f6669
devan <devanp92@ufl.edu>
2014-03-29 17:57:18 -0400

    Adding rule checking, test

---
+++ GameOfLife/src/cis4930/gameoflife/RuleChecking.java
@@ -0,0 +1,33 @@
+package cis4930.gameoflife;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by devan on 3/29/14.
+ */
+public class RuleChecking implements Runnable {
+
+    private Cell[][] grid = Grid.getGrid();
+
+
+    public List<Cell[][]> divideLists(int numThreads) {
+        List<Cell[][]> listOfSubListsSplitByNumThreads = new ArrayList<Cell[][]>();
+        int numCellsInTotalGrid = (int) Math.pow(grid.length, 2);
+        int beginningLength = 0;
+        for (int i = 1; i <= numThreads; i++) {
+            int endLength = i / numThreads * numCellsInTotalGrid;
+            Cell[][] list = Arrays.copyOfRange(grid, beginningLength, endLength);
+            listOfSubListsSplitByNumThreads.add(list);
+            beginningLength = endLength;
+        }
+        return listOfSubListsSplitByNumThreads;
+    }
+
+    
+    @Override
+    public void run() {
+
+    }
+}
---
+++ GameOfLife/src/test/RuleCheckingTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import cis4930.gameoflife.Cell;
+import cis4930.gameoflife.Grid;
+import cis4930.gameoflife.RuleChecking;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by devan on 3/29/14.
+ */
+public class RuleCheckingTest {
+    Grid grid;
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDivideLists() throws Exception {
+        grid = new Grid(4);
+        RuleChecking ruleChecking = new RuleChecking();
+        List<List<Cell>> lists = ruleChecking.divideLists(4);
+        assertNotNull(lists);
+    }
+}
