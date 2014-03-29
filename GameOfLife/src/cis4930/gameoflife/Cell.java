d9bc94048e31ecb7c605af0a683dbc19c6a11296
devan <devanp92@ufl.edu>
2014-03-29 16:45:30 -0400

    Adding Javadoc

--- GameOfLife/src/cis4930/gameoflife/Cell.java
+++ GameOfLife/src/cis4930/gameoflife/Cell.java
@@ -2,12 +2,17 @@ package cis4930.gameoflife;
 
 public class Cell extends QuadTreeElement {
 	boolean isAlive;
-	
+
 	public Cell(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
+    /**
+     * Set if the cell is alive or not.
+     * True if it is alive, False if dead.
+     * @param isAlive
+     */
 	public void setLife(boolean isAlive) {
 		this.isAlive = isAlive;
 	}
