2bd6f725783ae3f010bc87d1c7fdf5024c352e21
devan <devanp92@ufl.edu>
2014-04-05 21:57:40 -0400

    finding neighbors is done

--- GameOfLife/src/backend/NeighborFinder.java
+++ GameOfLife/src/backend/NeighborFinder.java
@@ -8,11 +8,26 @@ import java.util.concurrent.atomic.AtomicReference;
  * Created by devan on 4/5/14.
  */
 public class NeighborFinder extends CoordinateCalculator {
+    //TODO Finish the finding border and middle cells
+
+    private int CENTER_LEFT_NEIGHBOR;
+    private int CENTER_RIGHT_NEIGHBOR;
+
+    private int DOWN_LEFT_NEIGHBOR;
+    private int DOWN_RIGHT_NEIGHBOR;
+    private int DOWN_CENTER_NEIGHBOR;
+
+    private int UP_LEFT_NEIGHBOR;
+    private int UP_RIGHT_NEIGHBOR;
+    private int UP_CENTER_NEIGHBOR;
+
+
     Cell cell;
     AtomicReference[] grid = Grid.getGrid();
 
     public NeighborFinder(Cell cell) {
         this.cell = cell;
+        initializeCellsNeighborPositions();
     }
 
     public List<Cell> findNeighbors() {
@@ -45,6 +60,9 @@ public class NeighborFinder extends CoordinateCalculator {
             case BOTTOM_BORDER:
                 neighbors = findNeighborForBottomBorderCell();
                 break;
+            case MIDDLE:
+                neighbors = findNeighborForMiddleCells();
+                break;
         }
         return neighbors;
     }
@@ -52,11 +70,13 @@ public class NeighborFinder extends CoordinateCalculator {
     private List<Cell> findNeighborForTopLeftCornerCell() {
         List<Cell> list = new ArrayList<Cell>();
 
-        Cell rightNeighbor = (Cell) grid[2].get();
-        Cell bottomNeighbor = (Cell) grid[numRows].get();
+        Cell rightNeighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+        Cell bottomNeighbor = (Cell) grid[DOWN_RIGHT_NEIGHBOR].get();
+        Cell diagNeighbor = (Cell) grid[DOWN_RIGHT_NEIGHBOR].get();
 
         list.add(rightNeighbor);
         list.add(bottomNeighbor);
+        list.add(diagNeighbor);
 
         return list;
     }
@@ -64,23 +84,28 @@ public class NeighborFinder extends CoordinateCalculator {
     private List<Cell> findNeighborForTopRightCornerCell() {
         List<Cell> list = new ArrayList<Cell>();
 
-        Cell leftNeighbor = (Cell) grid[numRows - 2].get();
-        Cell bottomNeighbor = (Cell) grid[2 * numRows - 1].get();
+        Cell leftNeighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell bottomNeighbor = (Cell) grid[DOWN_CENTER_NEIGHBOR].get();
+        Cell diagNeighbor = (Cell) grid[DOWN_LEFT_NEIGHBOR].get();
 
-        list.add(leftNeighbor );
+        list.add(leftNeighbor);
         list.add(bottomNeighbor);
+        list.add(diagNeighbor);
 
         return list;
     }
 
     private List<Cell> findNeighborForBottomLeftCornerCell() {
         List<Cell> list = new ArrayList<Cell>();
-//
-//        Cell topNeighbor = (Cell) grid[numRows - 2].get();
-//        Cell rightNeighbor = (Cell) grid[2 * numRows - 1].get();
-//
-//        list.add(leftNeighbor );
-//        list.add(bottomNeighbor);
+
+        Cell topNeighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+        Cell diagNeighbor = (Cell) grid[UP_RIGHT_NEIGHBOR].get();
+        Cell rightNeighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+
+
+        list.add(topNeighbor);
+        list.add(rightNeighbor);
+        list.add(diagNeighbor);
 
         return list;
     }
@@ -88,11 +113,32 @@ public class NeighborFinder extends CoordinateCalculator {
     private List<Cell> findNeighborForBottomRightCornerCell() {
         List<Cell> list = new ArrayList<Cell>();
 
+        Cell leftNeighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell diagNeighbor = (Cell) grid[UP_LEFT_NEIGHBOR].get();
+        Cell topNeighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+
+
+        list.add(topNeighbor);
+        list.add(leftNeighbor);
+        list.add(diagNeighbor);
+
         return list;
     }
 
     private List<Cell> findNeighborForTopBorderCell() {
         List<Cell> list = new ArrayList<Cell>();
+
+        Cell center_left_neighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell center_right_neighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+        Cell down_left_neighbor = (Cell) grid[DOWN_LEFT_NEIGHBOR].get();
+        Cell down_right_neighbor = (Cell) grid[DOWN_RIGHT_NEIGHBOR].get();
+        Cell down_center_neighbor = (Cell) grid[DOWN_CENTER_NEIGHBOR].get();
+
+        list.add(center_left_neighbor);
+        list.add(center_right_neighbor);
+        list.add(down_left_neighbor);
+        list.add(down_right_neighbor);
+        list.add(down_center_neighbor);
 
         return list;
     }
@@ -100,11 +146,35 @@ public class NeighborFinder extends CoordinateCalculator {
     private List<Cell> findNeighborForRightBorderCell() {
         List<Cell> list = new ArrayList<Cell>();
 
+        Cell center_left_neighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell down_left_neighbor = (Cell) grid[DOWN_LEFT_NEIGHBOR].get();
+        Cell down_center_neighbor = (Cell) grid[DOWN_CENTER_NEIGHBOR].get();
+        Cell up_left_neighbor = (Cell) grid[UP_LEFT_NEIGHBOR].get();
+        Cell up_center_neighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+
+        list.add(center_left_neighbor);
+        list.add(down_left_neighbor);
+        list.add(down_center_neighbor);
+        list.add(up_left_neighbor);
+        list.add(up_center_neighbor);
+
         return list;
     }
 
     private List<Cell> findNeighborForLeftBorderCell() {
         List<Cell> list = new ArrayList<Cell>();
+
+        Cell center_right_neighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+        Cell down_right_neighbor = (Cell) grid[DOWN_RIGHT_NEIGHBOR].get();
+        Cell down_center_neighbor = (Cell) grid[DOWN_CENTER_NEIGHBOR].get();
+        Cell up_right_neighbor = (Cell) grid[UP_RIGHT_NEIGHBOR].get();
+        Cell up_center_neighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+
+        list.add(center_right_neighbor);
+        list.add(down_right_neighbor);
+        list.add(down_center_neighbor);
+        list.add(up_right_neighbor);
+        list.add(up_center_neighbor);
 
         return list;
     }
@@ -112,6 +182,58 @@ public class NeighborFinder extends CoordinateCalculator {
     private List<Cell> findNeighborForBottomBorderCell() {
         List<Cell> list = new ArrayList<Cell>();
 
+        Cell center_left_neighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell center_right_neighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+        Cell up_left_neighbor = (Cell) grid[UP_LEFT_NEIGHBOR].get();
+        Cell up_right_neighbor = (Cell) grid[UP_RIGHT_NEIGHBOR].get();
+        Cell up_center_neighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+
+        list.add(center_left_neighbor);
+        list.add(center_right_neighbor);
+        list.add(up_left_neighbor);
+        list.add(up_right_neighbor);
+        list.add(up_center_neighbor);
+
         return list;
+    }
+
+    private List<Cell> findNeighborForMiddleCells() {
+        List<Cell> list = new ArrayList<Cell>();
+
+        Cell center_left_neighbor = (Cell) grid[CENTER_LEFT_NEIGHBOR].get();
+        Cell center_right_neighbor = (Cell) grid[CENTER_RIGHT_NEIGHBOR].get();
+        Cell down_left_neighbor = (Cell) grid[DOWN_LEFT_NEIGHBOR].get();
+        Cell down_right_neighbor = (Cell) grid[DOWN_RIGHT_NEIGHBOR].get();
+        Cell down_center_neighbor = (Cell) grid[DOWN_CENTER_NEIGHBOR].get();
+        Cell up_left_neighbor = (Cell) grid[UP_LEFT_NEIGHBOR].get();
+        Cell up_right_neighbor = (Cell) grid[UP_RIGHT_NEIGHBOR].get();
+        Cell up_center_neighbor = (Cell) grid[UP_CENTER_NEIGHBOR].get();
+
+        list.add(center_left_neighbor);
+        list.add(center_right_neighbor);
+        list.add(down_left_neighbor);
+        list.add(down_right_neighbor);
+        list.add(down_center_neighbor);
+        list.add(up_left_neighbor);
+        list.add(up_right_neighbor);
+        list.add(up_center_neighbor);
+
+        return list;
+    }
+
+    public void initializeCellsNeighborPositions() {
+        int CELL_POSITION = super.convert2DCoordinateTo1D(cell.x, cell.y);
+
+        CENTER_LEFT_NEIGHBOR = CELL_POSITION - 1;
+        CENTER_RIGHT_NEIGHBOR = CELL_POSITION + 1;
+
+        DOWN_LEFT_NEIGHBOR = CELL_POSITION + numRows - 1;
+        DOWN_RIGHT_NEIGHBOR = CELL_POSITION + numRows + 1;
+        DOWN_CENTER_NEIGHBOR = CELL_POSITION + numRows;
+
+        UP_CENTER_NEIGHBOR = CELL_POSITION - numRows;
+        UP_LEFT_NEIGHBOR = CELL_POSITION - numRows - 1;
+        UP_RIGHT_NEIGHBOR = CELL_POSITION - numRows + 1;
+
     }
 }
