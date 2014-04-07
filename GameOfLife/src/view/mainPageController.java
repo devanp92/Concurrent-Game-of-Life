dba2e1f91dada9d74f8a5f54431c1df74fd2cd7d
danielffuentes <danielffuentes@gmail.com>
2014-04-07 02:16:03 -0400

    the board is 100% functional, need to work a little bit more on aesthetics but the functionality is done!!

--- GameOfLife/src/view/mainPageController.java
+++ GameOfLife/src/view/mainPageController.java
@@ -1,67 +1,115 @@
 package view;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.Node;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
- * Created by Daniel on 3/29/2014.
+ * Created by Daniel on 4/5/2014.
  */
+public class mainPageController implements Initializable{
 
-
-public class mainPageController {
-
-
-    public GridPane grid;
-    public Rectangle recta11;
-    public Rectangle recta12;
-    public Rectangle recta21;
-    public Rectangle recta22;
     public Label title;
+    public Label boardDimensionsLabel;
+    public TextField size;
+    public GridPane displayGrid;
+    public Label initializedBoardDimensionsLabel;
+    public Button startGameButton;
+    public Button pauseGameButton;
+    public Label statusLabel;
+    public Button resumeGameButton;
+    private int gridSize = 0;
+    private boolean gridclickedOn = false;
 
-    public void click()
-    {
-        ObservableList<Node> children = grid.getChildren();
-
-        System.out.println(children.indexOf(recta11));
-
-        if(grid.isGridLinesVisible())
-        {
-            grid.setGridLinesVisible(false);
+    public void startNewGame(ActionEvent event) {
+        if(!size.getText().isEmpty()) {
+            gridSize = Integer.parseInt(size.getText());
+            size.clear();
+            initializeBoard(gridSize);
+            inGameStatus();
+            statusLabel.setText("Click on board to start Game!");
+            statusLabel.setVisible(true);
         }
         else
         {
-            grid.setGridLinesVisible(true);
-        }
-    }
-    public void click1()
-    {
-        recta11.setFill(Color.BLACK);
-        recta11.setStroke(Color.BLACK);
-
-        if(recta11.getHeight() == 200)
-        {
-            recta11.setHeight(150);
-            recta11.setWidth(50);
-        }
-        else
-        {
-            recta11.setHeight(200);
-            recta11.setWidth(200);
+            statusLabel.setText("Please input a Grid size!!");
+            statusLabel.setVisible(true);
         }
     }
 
-
-    public void newGame(ActionEvent actionEvent) {
-
-    }
-
-    public void quit(ActionEvent actionEvent) {
+    public void quit() {
         System.exit(0);
+    }
+
+    private void initializeBoard(int size)
+    {
+       statusLabel.setVisible(false);
+       for(int i = 0; i < size; i++)
+       {
+            for (int j = 0; j <size; j++)
+            {
+                Rectangle recta = new Rectangle(20,20);
+                recta.setOnMouseClicked(new EventHandler<Event>(){
+                    @Override
+                    public void handle(Event event) {
+                        if(!gridclickedOn)
+                        {
+                            gridclickedOn = true;
+                            statusLabel.setText("The Game has started!!!");
+                            if (recta.getFill().equals(Color.BLACK))
+                            {
+                                recta.setFill(Color.WHITE);
+                            }
+                            else
+                            {
+                                recta.setFill(Color.BLACK);
+                            }
+                        }
+                    }
+                });
+                recta.setFill(Color.WHITE);
+                displayGrid.add(recta,i,j);
+            }
+        }
+        displayGrid.setVisible(true);
+        displayGrid.setMaxHeight(size*20);
+        displayGrid.setMaxWidth(size*20);
+    }
+    private void inGameStatus()
+    {
+        boardDimensionsLabel.setVisible(false);
+        size.setVisible(false);
+        initializedBoardDimensionsLabel.setVisible(true);
+        startGameButton.setVisible(false);
+        pauseGameButton.setVisible(true);
+        initializedBoardDimensionsLabel.setText(initializedBoardDimensionsLabel.getText() + gridSize +" X " + gridSize);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){}
+
+    public void pauseGame(ActionEvent event) {
+        //TO DO pause functionality needs to be implemented
+        statusLabel.setText("The game is paused");
+        pauseGameButton.setVisible(false);
+        resumeGameButton.setVisible(true);
+        System.out.println("pause functionality needs to be implemented");
+    }
+
+    public void resumeGame(ActionEvent event) {
+        resumeGameButton.setVisible(false);
+        pauseGameButton.setVisible(true);
+        statusLabel.setText("The game has resumed");
     }
 }
