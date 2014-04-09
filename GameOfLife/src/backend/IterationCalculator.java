cfc56eed6e40b624a44ff5f172fc979cea09c6fb
devan <devanp92@ufl.edu>
2014-04-08 22:21:10 -0400

    Creating the class that will calculate the next iteration

---
+++ GameOfLife/src/backend/IterationCalculator.java
@@ -0,0 +1,15 @@
+package backend;
+
+/**
+ * Created by devan on 4/8/14.
+ */
+public class IterationCalculator {
+
+    private Grid grid;
+
+    public IterationCalculator(Grid grid) {
+        this.grid = grid;
+    }
+
+
+}
