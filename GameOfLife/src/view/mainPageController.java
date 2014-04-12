531ba98d4fc36aa76d3a3eacd8b52818b3a16479
Daniel Fuentes <danielffuentes@gmail.com>
2014-04-11 21:01:00 -0400

    Still working on Main Page controller talking to the server to get board synced

--- GameOfLife/src/view/mainPageController.java
+++ GameOfLife/src/view/mainPageController.java
@@ -1,36 +1,47 @@
 package view;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-//import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import server.ClientConnection;
+import server.Server;
+import server.UICallback;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+
 
 /**
  * Created by Daniel on 4/5/2014.
  */
-public class mainPageController implements Initializable{
-
+public class mainPageController implements UICallback {
+    public Button connectButton;
+    ClientConnection connection = null;
     public Label title;
     public Label boardDimensionsLabel;
+    //private Map<String,Rectangle> gridMap;
     public TextField size;
     public GridPane displayGrid;
     public Label initializedBoardDimensionsLabel;
-    public Button startGameButton;
+    public Button buildGridButton;
     public Button pauseGameButton;
     public Label statusLabel;
     public Button resumeGameButton;
+    public TextField serverIpAddress;
     private int gridSize = 0;
-    private boolean gridclickedOn = false;
+    private boolean gridClickedOn = false;
 
     public void startNewGame(ActionEvent event) {
         if(!size.getText().isEmpty()) {
@@ -38,13 +49,11 @@ public class mainPageController implements Initializable{
             size.clear();
             initializeBoard(gridSize);
             inGameStatus();
-            statusLabel.setText("Click on board to start Game!");
-            statusLabel.setVisible(true);
+            setStatusLabel("Click on board to start Game!");
         }
         else
         {
-            statusLabel.setText("Please input a Grid size!!");
-            statusLabel.setVisible(true);
+            setStatusLabel("Please input a Grid size!!");
         }
     }
 
@@ -55,17 +64,17 @@ public class mainPageController implements Initializable{
     private void initializeBoard(int size)
     {
        statusLabel.setVisible(false);
-       for(int i = 0; i < size; i++)
+       for(Integer i = 0; i < size; i++)
        {
-            for (int j = 0; j <size; j++)
+            for (Integer j = 0; j <size; j++)
             {
                 final Rectangle recta = new Rectangle(20,20);
                 recta.setOnMouseClicked(new EventHandler<Event>(){
                     @Override
                     public void handle(Event event) {
-                        if(!gridclickedOn)
+                        if(!gridClickedOn)
                         {
-                            gridclickedOn = true;
+                            gridClickedOn = true;
                             statusLabel.setText("The Game has started!!!");
                             if (recta.getFill().equals(Color.BLACK))
                             {
@@ -90,14 +99,15 @@ public class mainPageController implements Initializable{
     {
         boardDimensionsLabel.setVisible(false);
         size.setVisible(false);
+        buildGridButton.setVisible(false);
         initializedBoardDimensionsLabel.setVisible(true);
-        startGameButton.setVisible(false);
         pauseGameButton.setVisible(true);
         initializedBoardDimensionsLabel.setText(initializedBoardDimensionsLabel.getText() + gridSize +" X " + gridSize);
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){}
+    private void setStatusLabel(String newSatus){
+        statusLabel.setVisible(true);
+        statusLabel.setText(newSatus);
+    }
 
     public void pauseGame(ActionEvent event) {
         //TO DO pause functionality needs to be implemented
@@ -112,5 +122,37 @@ public class mainPageController implements Initializable{
         pauseGameButton.setVisible(true);
         statusLabel.setText("The game has resumed");
         System.out.println("resume functionality needs to be implemented");
+    }
+
+    public void startConnection(ActionEvent actionEvent)
+    {
+        try {
+            connection = new ClientConnection(new Socket(serverIpAddress.getText(), Server.port));
+            connection.subscribe(this);
+            setStatusLabel("Server Connection Started");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateGame() {
+        for(int i = 0; i < gridSize; i++)
+        {
+            for(int j = 0; j < gridSize; j++)
+            {
+                if(connection.getGrid().convertGridTo2DArray()[i][j].getCellState() == 0)
+                {
+                    Rectangle rectangle = (Rectangle) displayGrid.getChildren().get((i*gridSize)+ j);
+                    rectangle.setFill(Color.WHITE);
+                }
+                else
+                {
+                    Rectangle rectangle = (Rectangle) displayGrid.getChildren().get((i*gridSize)+ j);
+                    rectangle.setFill(Color.BLACK);
+                }
+            }
+        }
     }
 }
--- GameOfLife/src/view/startGame.java
+++ GameOfLife/src/view/startGame.java
@@ -6,22 +6,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import server.ClientConnection;
+import server.Server;
+
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Created by Daniel on 3/29/2014.
  */
 /*This class should hold the client connection Object try to see how to make it happen when the user presses the connect button */
 public class startGame extends Application{
+
     public static void main(String[] args) {
+
         launch(args);
+
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
         //root.getStylesheets().add("sample/myStyle.css");
         primaryStage.setTitle("Game of Life");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 }
